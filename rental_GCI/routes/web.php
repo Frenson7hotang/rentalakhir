1829bde3255a601658bc325223847f7d2997bcb2
Ruben Febrian <rubenfebrian1202@gmail.com>
2023-05-06 21:52:00 +0700

    laravel

--- rental_GCI/routes/web.php
+++ rental_GCI/routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MobilController;
 use App\Http\Controllers\TypeController;
+use App\Http\Controllers\LaporanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,7 +30,7 @@ Route::get('/tambahmobil', [MobilController::class,'tambahmobil'])->name('tambah
 Route::post('/tambahdata', [MobilController::class,'tambahdata'])->name('tambahdata');
 Route::get('/tampilkandata/{id}', [MobilController::class,'tampilkandata'])->name('tampilkandata');
 Route::post('/perbaharuidata/{id}', [MobilController::class,'perbaharuidata'])->name('perbaharuidata');
-Route::get('/hapus/{id}', [MobilController::class,'hapus'])->name('hapus');
+Route::get('/hapus_mobil/{id}', [MobilController::class,'hapus'])->name('hapus_mobil');
 
 Route::get('/login', [LoginController::class,'login'])->name('login');
 Route::post('/loginproses', [LoginController::class,'loginproses'])->name('loginproses');
@@ -39,12 +40,24 @@ Route::post('/registeruser', [LoginController::class,'registeruser'])->name('reg
 
 Route::get('/logout', [LoginController::class,'logout'])->name('logout');
 
-Route::get('/exportpdf', [MobilController::class,'exportpdf'])->name('exportpdf');
+//pdf
+Route::get('/exportpdfmobil', [MobilController::class,'exportpdfmobil'])->name('exportpdfmobil');
+Route::get('/exportpdf', [LaporanController::class, 'exportpdf'])->name('exportpdf');
 
-Route::get('/type_mobil', [TypeController::class, 'type_mobil']);
-Route::get('/tambah_type', [TypeController::class, 'tambah_type']);
-Route::post('/tambah', [TypeController::class, 'tambah']);
-Route::get('/edit/{id}', [TypeController::class, 'edit']);
-Route::post('/update/{id}', [TypeController::class, 'update']);
-Route::get('/hapus/{id}', [TypeController::class, 'delete']);
+Route::get('/type_mobil', [TypeController::class, 'type_mobil'])->name('type_mobil');
+Route::get('/tambah_type', [TypeController::class, 'tambah_type'])->name('tambah_type');
+Route::post('/tambah', [TypeController::class, 'tambah'])->name('tambah');
+Route::get('/edit_type/{id}', [TypeController::class, 'edit_type'])->name('edit_type');
+Route::post('/update_type/{id}', [TypeController::class, 'update_type'])->name('update_type');
+Route::get('/hapus_type/{id}', [TypeController::class, 'delete_type'])->name('delete_type');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/laporan', [LaporanController::class, 'laporan']);
+Route::get('/tambahlaporan', [LaporanController::class, 'tambahlaporan'])->name('tambahlaporan');
+Route::post('/tambahdatalaporan', [LaporanController::class, 'tambahdatalaporan'])->name('tambahdatalaporan');
+Route::get('/edit_laporan/{id}', [LaporanController::class, 'edit_laporan']);
+Route::post('/update/{id}', [LaporanController::class, 'update']);
+Route::get('/hapus_laporan/{id}', [LaporanController::class, 'delete_laporan']);
+
 
